b1365e229a508d958db4e965114ae7c9751e9a00
matyi697 <wittmann.matyas@gmail.com>
2024-12-02 15:17:20 +0100

    Day 1 	new file:   Day 1/input.txt 	new file:   Day 1/task1.cpp 	new file:   Day 1/task2.cpp

---
+++ Day 1/task1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream> 
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    ifstream file(argv[1]);
+    vector<int> colA;
+    vector<int> colB;
+    string line;
+    unsigned ans = 0;
+
+    while (getline(file, line)) {
+        istringstream iss(line);
+        int tempA, tempB;
+        if (iss >> tempA >> tempB) {
+            colA.push_back(tempA);
+            colB.push_back(tempB);
+        } 
+    }
+
+    file.close();
+
+    sort(colA.begin(), colA.end());
+    sort(colB.begin(), colB.end());
+
+    for (int a = 0; a < colA.size(); a++) 
+        ans += abs(colA[a] - colB[a]); 
+    
+    cout << ans << "\n";
+    return 0;
+}
