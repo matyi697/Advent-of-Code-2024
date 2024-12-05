48f3cf34e5ed74f67b7de764de229e18d3070225
matyi697 <wittmann.matyas@gmail.com>
2024-12-05 18:33:08 +0100

    Day 4 	modified:   Day 4/task2.cpp 	deleted:    Day 4/test.txt

--- Day 4/task2.cpp
+++ Day 4/task2.cpp
@@ -4,21 +4,30 @@
 
 using namespace std;
 
-const char word[5] = "XMAS";
-
 unsigned checkXmas(const vector<vector<char>>& input, int x0, int y0) {
-    unsigned ans = 0;
     bool right = x0 <= input[0].size() - 4;
     bool left = x0 >= 3;
     bool up = y0 >= 3;
     bool down = y0 <= input.size() - 4;
 
-    /*
-    minden x kozepe a betu
-        -4 lehetoseg lehet hrdcodeolni    
-    */
+    if (input[x0][y0] != 'A')
+        return 0;
+    
+    bool leftDiag = false;
+    bool rightDiag = false;
+    char topLeft = (x0-1 >= 0 && y0-1 >= 0) ? input[x0-1][y0-1] : 0;
+    char bottomLeft = (x0-1 >= 0 && y0+1 < input.size()) ? input[x0-1][y0+1] : 0;
+    char topRight = (x0+1 < input[0].size() && y0-1 >= 0) ? input[x0+1][y0-1] : 0;
+    char bottomRight = (x0+1 < input[0].size() && y0+1 < input.size()) ? input[x0+1][y0+1] : 0;
+   
 
-    return ans;
+    if ((topLeft == 'M' && bottomRight == 'S') || (topLeft == 'S' && bottomRight == 'M'))
+        leftDiag = true;
+
+    if ((topRight == 'M' && bottomLeft == 'S') || (topRight == 'S' && bottomLeft == 'M'))
+        rightDiag = true;
+
+    return leftDiag && rightDiag;
 }
 
 int main (int argc, char* argv[]) {
