47176ad2769d44a7b5cd69d5b1f556e12b805154
matyi697 <wittmann.matyas@gmail.com>
2024-12-09 01:06:37 +0100

    Day 7 	modified:   Day 7/task2.cpp 	deleted:    Day 7/test.txt

--- Day 7/task2.cpp
+++ Day 7/task2.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <cmath>
 
 using namespace std;
 typedef unsigned long long u64;
@@ -10,14 +11,16 @@ typedef unsigned long long u64;
 u64 evaluateExpression(vector<int> numbers, vector<int> operators) {
     u64 ans = numbers[0];
     for(int i = 0; i < numbers.size()-1; i++) {
-        if (operators[i] == 0) {
-            ans += numbers[i+1];
-        } else
-        if (operators[i] == 1) {
-            ans *= numbers[i+1];
-        } else
-        if (operators[i] == 2) {
-            ans *= numbers[i+1];
+        switch (operators[i]) {
+            case 0:
+                ans += numbers[i+1];
+            break;
+            case 1:
+                ans *= numbers[i+1];
+            break;
+            case 2:
+                ans = pow(10, floor(log10(numbers[i + 1])) + 1) * ans + numbers[i + 1];
+            break;
         }
     }
     return ans;
@@ -25,23 +28,19 @@ u64 evaluateExpression(vector<int> numbers, vector<int> operators) {
 
 void stepOperator(std::vector<int>& opr) {
     int n = opr.size();
-    for (int i = n - 1; i >= 0; --i) { 
-        if (opr[i] == 0) {
-            opr[i] = 1; 
-            return;
-        } else
-            opr[i] = 0;
+    for (int i = n - 1; i >= 0; --i) {
+        opr[i] = (opr[i] + 1) % 3; 
+        if (opr[i] != 0) break;
     }
 }
 
 bool isPossible(u64 sum, vector<int> numbers) {
     vector<int> operators(numbers.size()-1, 0);
-    int maxIter = 1 << (numbers.size()-1);
+    int maxIter = pow(3, numbers.size() - 1);
 
     for (int i = 0; i < maxIter; ++i) {
-        if (evaluateExpression(numbers, operators) == sum) {
+        if (evaluateExpression(numbers, operators) == sum) 
             return 1;
-        }
         stepOperator(operators);
     }
     return 0;
